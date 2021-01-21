1ba4c86cf853ef4c16cce0b1ce4a1f38e4023d3d
perajmar <Marko.Perajica@maurer-electronics.hr>
2021-01-21 11:13:30 +0100

    Initial commit

---
+++ backend/administrator/src/test/java/smart_restaurant/administrator/AdministratorApplicationTests.java
@@ -0,0 +1,13 @@
+package smart_restaurant.administrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/central/src/main/java/smart_restaurant/central/CentralApplication.java
@@ -0,0 +1,13 @@
+package smart_restaurant.central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralApplication.class, args);
+	}
+
+}
